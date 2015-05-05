76c07a99c40fea280ddffd7ed88fea597970118a
songzigw <songzigw@163.com>
2015-05-06 01:52:24 +0800

    n

--- src/com/song/demo/net/NetTest.java
+++ src/com/song/demo/net/NetTest.java
@@ -4,18 +4,20 @@ import java.io.IOException;
 
 import org.apache.commons.httpclient.Header;
 import org.apache.commons.httpclient.HttpClient;
+import org.apache.commons.httpclient.HttpException;
 import org.apache.commons.httpclient.HttpMethod;
 import org.apache.commons.httpclient.NameValuePair;
 import org.apache.commons.httpclient.methods.GetMethod;
 import org.apache.commons.httpclient.methods.PostMethod;
-import org.apache.commons.httpclient.params.HttpMethodParams;
 
 public class NetTest {
-	public static void main(String[] args) throws IOException {
+
+	public static void a() throws HttpException, IOException {
 		HttpClient client = new HttpClient();
-		//client.getHostConfiguration().setHost("www.imobile.com.cn", 80, "http");
-		HttpMethod method = getGetMethod();// 使用POST方式提交数据
+		HttpMethod method = new GetMethod(
+				"http://192.168.1.5:8080/song-account-web/api/user/getUser.json");
 		client.executeMethod(method);
+
 		// 打印服务器返回的状态
 		System.out.println(method.getStatusLine());
 		System.out.println(method.getStatusText());
@@ -24,12 +26,9 @@ public class NetTest {
 		System.out.println(method.getRequestHeader("Host").getValue());
 		System.out.println(method.getPath());
 		System.out.println(method.getHostAuthState().toString());
-		
 		System.out.println(method.getQueryString());
-		//System.out.println(method.getResponseBodyAsString());
-		HttpMethodParams m = method.getParams();
-		System.out.println();
-		System.out.println();
+		// System.out.println(method.getResponseBodyAsString());
+		// HttpMethodParams m = method.getParams();
 		Header[] hs1 = method.getRequestHeaders();
 		for (Header h : hs1) {
 			System.out.println(h.getName());
@@ -40,15 +39,15 @@ public class NetTest {
 			System.out.println(h.getName());
 		}
 		// 打印结果页面
-		//String response = new String(method.getResponseBodyAsString().getBytes(
-		//		"8859_1"));
+		String response = new String(method.getResponseBodyAsString().getBytes(
+				"UTF-8"));
 		// 打印返回的信息
-		//System.out.println(response);
+		System.out.println(response);
 		method.releaseConnection();
 	}
 
-	private static HttpMethod getGetMethod() {
-		return new GetMethod("http://xzheg.com:8080/ps.html?catId=3");
+	public static void main(String[] args) throws IOException {
+		a();
 	}
 
 	private static HttpMethod getPostMethod() {
