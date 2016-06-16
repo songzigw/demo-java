8c43e352faa82665da4bf90c131ca2b39d8739bf
songzigw <songzigw@163.com>
2016-06-16 18:08:41 +0800

    reflection

---
+++ src/main/java/com/song/demo/reflection/InstanceOf.java
@@ -0,0 +1,25 @@
+/*
+ *  Copyright (c) 2016, zhangsong<songm.cn>.
+ *
+ */
+
+package com.song.demo.reflection;
+
+/**
+ * 操作符instanceof，用于在运行时判断对象的类型，
+ * 比如在equals(Object obj)方法中，判断参数obj
+ * 对象是否与本对象类型相同。<br/>
+ * 
+ * <p>
+ * 1.instanceof操作符左边是具体对象标识符，右边是
+ * 类的标识符，返回类型为布尔值。<br/>
+ * 2.类的对象与类做instanceof操作时，结果为true；
+ * 子类对象与父类做instanceof运算，结果为ture；
+ * 其他情况下返回false。<br/>
+ * 
+ * @author zhangsong
+ *
+ */
+public class InstanceOf {
+
+}
